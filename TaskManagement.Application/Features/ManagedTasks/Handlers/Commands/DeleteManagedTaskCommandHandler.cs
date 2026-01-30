a055b762f149e4ebf5bb33d0f0bc270b7edb9199
Charith Karunarathne <charith.karun@sygnal.tech>
2026-01-30 22:06:40 +0530

    Change

--- TaskManagement.Application/Features/ManagedTasks/Handlers/Commands/DeleteManagedTaskCommandHandler.cs
+++ TaskManagement.Application/Features/ManagedTasks/Handlers/Commands/DeleteManagedTaskCommandHandler.cs
@@ -1,10 +1,5 @@
 ﻿using AutoMapper;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaskManagement.Application.Features.ManagedTasks.Requests.Commands;
 using TaskManagement.Application.Contracts.Persistence;
 
